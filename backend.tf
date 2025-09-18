bfbd5c586d11df59a5ba4a28c95e5bcdd261ada1
SoumyajitSaha1999 <129038520+SoumyajitSaha1999@users.noreply.github.com>
2025-09-18 11:52:57 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 terraform {
   backend "s3" {
-    bucket         = "reya-tfstate-bucket-123"
+    bucket         = "soumya-tfstate-bucket-897"
     key            = "3tier/prod/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "dynamodb-terraform-state-lock"
